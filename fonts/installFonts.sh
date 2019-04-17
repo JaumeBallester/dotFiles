01c643e843bff6358d5253e94e4dce128c45cf29
Jaume <jballesterpuig@gmail.com>
2019-04-17 17:54:42 +0200

    Improvements install Fonts

--- fonts/installFonts.sh
+++ fonts/installFonts.sh
@@ -6,5 +6,29 @@ if [ ! -d $HOME/.fonts ]; then
   mkdir -p $HOME/.fonts;
 fi
 
+
+
+DEST="$HOME/.fonts"
+EXT="otf"
+
 #copy the fonts to the user fonts folder
-cp $HOME/.config/fonts/*.oft $HOME/.fonts
+cd $HOME/.config/fonts
+
+for i in *."$EXT"; do
+	sudo cp -iv "$i" "$DEST"
+done
+
+
+
+
+echo
+echo
+echo "Updating the font cache..."
+sudo fc-cache -fv
+
+if [ $? != 0 ]; then
+    echo "Error updating the font cache. Your fonts haven't been completely installed. Try running sudo fc-cache -fv manually. Exiting..."
+    echo -n "Press <Enter> to continue."
+    read FSF
+    exit $?
+fi
