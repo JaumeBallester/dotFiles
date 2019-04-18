c39eed95a302b0d18d94439822564d2137386f25
Jaume <jballesterpuig@gmail.com>
2019-04-18 18:19:51 +0200

    lock icon

--- i3/lockscreen.sh
+++ i3/lockscreen.sh
@@ -2,4 +2,4 @@
 
 #takes a screenshot, blurs it, and adds the png above
 
-i3lock -i <(import -silent -window root png:- | mogrify -blur 0x8 png:- | composite -gravity South -geometry -20x1200 $HOME/Pictures/Rick.png png:- png:-)
+i3lock -i <(import -silent -window root png:- | mogrify -blur 0x8 png:- | composite -gravity Center  $HOME/.config/i3/Rick.png png:- png:-)
