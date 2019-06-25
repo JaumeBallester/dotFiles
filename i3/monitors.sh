5253d3c132001f45b3495e57bd9c5836e1b097c9
mar <marb.gstocks@gmail.com>
2019-06-25 09:47:41 +0200

    Added new music shortcuts, set wallpaper and set keyboard layout on reboot. Also changed some colors.

--- i3/monitors.sh
+++ i3/monitors.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DVI-D-1 --off --output HDMI-1 --mode 2560x1440 --pos 2560x0 --rotate normal
+xrandr --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DVI-D-1 --off --output HDMI-1 --mode 2560x1440 --pos 2560x0 --rotate normal
