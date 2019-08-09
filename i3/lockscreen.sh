91661a50edfb9ad7227f14fb4fb93add0b0e7ef5
mar <marb.gstocks@gmail.com>
2019-08-09 15:38:48 +0200

    New lockscreen

--- i3/lockscreen.sh
+++ i3/lockscreen.sh
@@ -2,10 +2,17 @@
 
 #takes a screenshot, pixelate it, and adds the png above
 
-ICON=$HOME/.config/i3/lock.png
+ICON=$HOME/.config/i3/marvel.png
 TMPBG=/tmp/screen.png
 scrot /tmp/screen.png
-convert $TMPBG -scale 10% -scale 1000% $TMPBG
-convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+
+# For pixelating
+    #convert $TMPBG -scale 10% -scale 1000% $TMPBG
+
+# For blur
+    convert $TMPBG -blur 0x8 $TMPBG
+
+convert $TMPBG $ICON -geometry +995+366 -compose difference -composite $TMPBG
+convert $TMPBG $ICON -geometry +3555+366 -compose difference -composite $TMPBG
 
 i3lock -u -i $TMPBG
